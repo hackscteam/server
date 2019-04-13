73414a077328cdba4821725173c4a0d63961bfce
Zachary Pinto <zpinto@uci.edu>
2019-04-12 23:53:30 -0700

    fix base

---
+++ app.py
@@ -0,0 +1,10 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Hello World!</h1>"
