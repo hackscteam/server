d243c6818b989c041912c7ba8a7d56d105b73216
Zachary Pinto <zpinto@uci.edu>
2019-04-13 22:51:39 -0700

    fix this shit

---
+++ main.py
@@ -0,0 +1,180 @@
+from flask import Flask, request, jsonify
+import flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, DateTime
+import json
+import datetime
+import os
+import socket
+
+app = Flask(__name__)
+
+
+def is_ipv6(addr):
+    """Checks if a given address is an IPv6 address."""
+    try:
+        socket.inet_pton(socket.AF_INET6, addr)
+        return True
+    except socket.error:
+        return False
+
+
+# Environment variables are defined in app.yaml.
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "User"
+
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    mobile = db.Column(db.String(80))
+    address = db.Column(db.String(80))
+    birthday = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    created_date = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.first_name
+
+
+class Pending(db.Model):
+    __tablename__ = "Pending"
+
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    user1ID = db.Column(db.Integer)
+    user2ID = db.Column(db.Integer)
+    created_date = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+
+class Wave(db.Model):
+    __tablename__ = "Wave"
+
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    userID = db.Column(db.Integer)
+    lat = db.Column(db.Integer)
+    lon = db.Column(db.Integer)
+    created_date = db.Column(DateTime, default=datetime.datetime.utcnow)
+    recieveKey = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User %r>' % self.time
+
+
+@app.after_request
+def allow_cross_domain(response: flask.Response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'content-type'
+    return response
+
+
+@app.route('/', methods=['GET'])
+def home():
+
+    return "Test"
+
+
+@app.route('/v1/test', methods=['GET'])
+def test():
+    wave = Wave.query.filter_by(id=0).first()
+    return jsonify({'test': wave.id})
+
+
+def getBestMatch(posMatches, timeCheck, checkID):
+    print("gettingMatch")
+    for wave in posMatches:
+        print("gettingMatch")
+        diff = wave.created_date - timeCheck
+        print(diff.total_seconds())
+        diff = diff.total_seconds()
+        if abs(diff) <= 45 and wave.userID is not checkID:
+            print(diff)
+            return wave
+
+
+@app.route('/v1/waveaction', methods=['POST'])
+def makeWave():
+        #waveMatch = getBestMatch(Wave.query.all(), timeCheck)
+
+    ra = request.get_json()
+    print(ra)
+    if ra['id'] and ra['lat'] and ra['long'] and ra['time']:
+        timeCheck = datetime.datetime.utcnow()
+        waveMatch = getBestMatch(
+            Wave.query.all(), timeCheck, ra['id'])
+        if waveMatch:
+            recieveKey = waveMatch.recieveKey
+            # db.session.delete(waveMatch)
+            # db.session.commit()
+            pend = Pending.query.filter_by(id=recieveKey).first()
+            pend.user2ID = ra['id']
+            db.session.commit()
+            return jsonify({'recieveKey': recieveKey})
+        else:
+            pending = Pending(user1ID=ra['id'])
+            db.session.add(pending)
+            db.session.commit()
+            newWave = Wave(
+                userID=ra['id'], lat=ra['lat'], lon=ra['long'], recieveKey=pending.id)
+            db.session.add(newWave)
+            db.session.commit()
+            return jsonify({'recieveKey': newWave.recieveKey})
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+
+@app.route('/v1/getcontact', methods=['POST'])
+def getContact():
+    ra = request.get_json()
+    print(ra)
+    if ra['userid'] and ra['recieveKey']:
+        pend = Pending.query.filter_by(id=ra['recieveKey']).first()
+        if(pend.user1ID is None or pend.user2ID is None):
+            print({'firstname': '~There is no one around!',
+                   'user1': pend.user1ID,
+                   'user2': pend.user2ID})
+            return jsonify({'firstname': '~There is no one around!',
+                            'user1': pend.user1ID,
+                            'user2': pend.user2ID})
+        if(pend.user1ID == ra['userid']):
+            contact = User.query.filter_by(id=pend.user2ID).first()
+        else:
+            contact = User.query.filter_by(id=pend.user1ID).first()
+
+        return jsonify({'firstname': contact.first_name,
+                        'lastname': contact.last_name,
+                        'mobile': contact.mobile,
+                        'address': contact.address,
+                        'birthdate': contact.birthday,
+                        'email': contact.email
+                        })
+
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+
+@app.route('/v1/createuser', methods=['POST'])
+def write():
+
+    # print(request.body)
+    print('good')
+    rd = request.get_json()
+
+    print(rd)
+    newUser = User(first_name=rd['firstname'], last_name=rd['lastname'], mobile=rd['mobile'],
+                   email=rd['email'], address=rd['address'], birthday=rd['birthdate'])
+    db.session.add(newUser)
+    db.session.commit()
+
+    return jsonify({'id': newUser.id})
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
